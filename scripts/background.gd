571e97768352b45820ec0610c74232216ea9fbfb
kolumpi <kolumban@eclipso.at>
2025-11-13 18:28:00 +0100

    Update 4

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,10 +1,9 @@
 extends TextureRect
 
-@export var scroll_speed: Vector2 = Vector2(-50, 0)  # Pixel pro Sekunde
+@export var scroll_speed: Vector2 = Vector2(-50, 0)  # Pixel/Sek
 
 func _process(delta):
 	position += scroll_speed * delta
 
-	# Optional: Reset, wenn es zu weit verschoben ist
 	if position.x <= -texture.get_width():
 		position.x += texture.get_width()
