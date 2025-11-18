4e98d96a40095e4192c8fb7878d5de88b395e7d6
kolumpi <kolumban@eclipso.at>
2025-11-18 14:38:02 +0100

    Update 9

---
+++ scripts/cow.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var move_speed = Global.obstacleBaseSpeed * randf_range(0.5, 1.5)
+
+func _process(delta):
+	position.x -= move_speed * delta
+
+	# Wenn das Hindernis links raus ist, löschen 
+	if position.x < -200:
+		queue_free()
+		
+	if Global.alive == false:
+		queue_free()
